ff81051cd525a7567ed0402c51d99373b77242ba
faithwest <faithchepkoech425@gmail.com>
2024-01-10 15:44:18 +0300

    assignment started

--- server/app.py
+++ server/app.py
@@ -23,12 +23,21 @@ def bakeries():
     bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
     return make_response(  bakeries,   200  )
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods=['GET', 'PATCH'])
 def bakery_by_id(id):
-
+    
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
     return make_response ( bakery_serialized, 200  )
+    bakery = Bakery.query.get_or_404(id)
+
+    data = request.form
+    for attr, value in data.items():
+        setattr(bakery, attr, value)
+
+    db.session.commit()
+    
+    return jsonify(bakery.to_dict()), 200   
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -44,6 +53,42 @@ def most_expensive_baked_good():
     most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
+
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    data = request.form
+    baked_good = BakedGood(
+        name=data.get("name"),
+        price=data.get("price"),
+        bakery_id=data.get("bakery_id")
+    )
+
+    db.session.add(baked_good)
+    db.session.commit()
+
+    return jsonify(baked_good.to_dict()), 201
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.get_or_404(id)
+
+    data = request.form
+    for attr, value in data.items():
+        setattr(bakery, attr, value)
+
+    db.session.commit()
+
+    return jsonify(bakery.to_dict()), 200
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.get_or_404(id)
+
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    return jsonify({'message': 'Record successfully deleted'}), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
